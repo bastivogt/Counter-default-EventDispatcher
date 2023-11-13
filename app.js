6d98ef1451fd1253037a546bfccd8d54a0c2cb3e
Sevo <betont.online@googlemail.com>
2023-11-13 01:17:52 +0100

    some changes

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ console.log("app.js");
 
 const counter = new Counter();
 
+function counterChangeHandler(evt) {
+  console.log(Counter.COUNTER_CHANGE, "count:", evt.detail.count);
+}
+
 counter.addEventListener(Counter.COUNTER_START, (evt) => {
   console.log(Counter.COUNTER_START, "count:", evt.detail.count);
 });
 
-counter.addEventListener(Counter.COUNTER_CHANGE, (evt) => {
+/*counter.addEventListener(Counter.COUNTER_CHANGE, (evt) => {
   console.log(Counter.COUNTER_CHANGE, "count:", evt.detail.count);
-});
+});*/
+
+counter.addEventListener(Counter.COUNTER_CHANGE, counterChangeHandler);
 
 counter.addEventListener(Counter.COUNTER_FINISH, (evt) => {
   console.log(Counter.COUNTER_FINISH, "count:", evt.detail.count);
   console.log("evt:", evt);
 });
+
+//counter.removeEventListener(Counter.COUNTER_CHANGE, counterChangeHandler);
 
 counter.run();
